b233270199a986804a61f647ac427bfe0d89bda7
JUMAN DAS <jumand909@gmail.com>
2024-06-10 23:27:27 +0530

    backend deploy

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
+import cors from 'cors';
 dotenv.config();
 
 console.log(process.env.MONGO);

8b36e36e9f3e5dfac1ac0c9b2a7296c2f12730d2
JUMAN DAS <jumand909@gmail.com>
2024-06-11 02:28:07 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -37,10 +37,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join('frontend', '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join('frontend', 'client', 'dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {

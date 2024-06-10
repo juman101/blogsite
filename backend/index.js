ae6122407a3898cb6f11b34814676910f9afe197
JUMAN DAS <jumand909@gmail.com>
2024-06-11 01:37:05 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is running on port 3000!');
 });
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://blogsite-0amv.onrender.com',
         secure: false,
       },
     },
